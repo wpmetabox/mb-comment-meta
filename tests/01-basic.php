4020ec8d784581425b0dfae86fa7505e420251c1
Anh Tran <anhtnt@elightup.com>
2024-10-14 16:40:59 +0700

    Update PCP

--- tests/01-basic.php
+++ tests/01-basic.php
@@ -10,48 +10,48 @@ add_filter( 'rwmb_meta_boxes', function ( $meta_boxes ) {
 	$prefix = 'your_prefix_';
 
 	$meta_boxes[] = array(
-		'title'  => esc_html__( 'Standard Fields', 'your-prefix' ),
+		'title'  => esc_html__( 'Standard Fields', 'mb-comment-meta' ),
 		'type'   => 'comment',
 		'fields' => array(
 			array(
-				'name'              => esc_html__( 'Text', 'your-prefix' ),
-				'label_description' => esc_html__( 'Some description', 'your-prefix' ),
+				'name'              => esc_html__( 'Text', 'mb-comment-meta' ),
+				'label_description' => esc_html__( 'Some description', 'mb-comment-meta' ),
 				'id'                => "{$prefix}text",
-				'desc'              => esc_html__( 'Text description', 'your-prefix' ),
+				'desc'              => esc_html__( 'Text description', 'mb-comment-meta' ),
 				'type'              => 'text',
-				'std'               => esc_html__( 'Default text value', 'your-prefix' ),
+				'std'               => esc_html__( 'Default text value', 'mb-comment-meta' ),
 				'clone'             => true,
 			),
 			array(
-				'name' => esc_html__( 'Checkbox', 'your-prefix' ),
+				'name' => esc_html__( 'Checkbox', 'mb-comment-meta' ),
 				'id'   => "{$prefix}checkbox",
 				'type' => 'checkbox',
 				'std'  => 1,
 			),
 			array(
-				'name'    => esc_html__( 'Radio', 'your-prefix' ),
+				'name'    => esc_html__( 'Radio', 'mb-comment-meta' ),
 				'id'      => "{$prefix}radio",
 				'type'    => 'radio',
 				'options' => array(
-					'value1' => esc_html__( 'Label1', 'your-prefix' ),
-					'value2' => esc_html__( 'Label2', 'your-prefix' ),
+					'value1' => esc_html__( 'Label1', 'mb-comment-meta' ),
+					'value2' => esc_html__( 'Label2', 'mb-comment-meta' ),
 				),
 			),
 			array(
-				'name'        => esc_html__( 'Select', 'your-prefix' ),
+				'name'        => esc_html__( 'Select', 'mb-comment-meta' ),
 				'id'          => "{$prefix}select",
 				'type'        => 'select',
 				'options'     => array(
-					'value1' => esc_html__( 'Label1', 'your-prefix' ),
-					'value2' => esc_html__( 'Label2', 'your-prefix' ),
+					'value1' => esc_html__( 'Label1', 'mb-comment-meta' ),
+					'value2' => esc_html__( 'Label2', 'mb-comment-meta' ),
 				),
 				'multiple'    => false,
 				'std'         => 'value2',
-				'placeholder' => esc_html__( 'Select an Item', 'your-prefix' ),
+				'placeholder' => esc_html__( 'Select an Item', 'mb-comment-meta' ),
 			),
 			array(
-				'name' => esc_html__( 'Textarea', 'your-prefix' ),
-				'desc' => esc_html__( 'Textarea description', 'your-prefix' ),
+				'name' => esc_html__( 'Textarea', 'mb-comment-meta' ),
+				'desc' => esc_html__( 'Textarea description', 'mb-comment-meta' ),
 				'id'   => "{$prefix}textarea",
 				'type' => 'textarea',
 				'cols' => 20,
@@ -75,10 +75,10 @@ add_action( 'wp_head', function () {
 		echo '<h1>Field: ', esc_html( $field ), '</h1>';
 		echo '<p><code>rwmb_meta()</code></p>';
 		$value = rwmb_meta( $prefix . $field, [ 'object_type' => 'comment' ], $comment_id );
-		var_dump( $value );
+		var_dump( $value ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
 		echo '<p><code>rwmb_get_value()</code></p>';
 		$value = rwmb_get_value( $prefix . $field, [ 'object_type' => 'comment' ], $comment_id );
-		var_dump( $value );
+		var_dump( $value ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
 		echo '<p><code>rwmb_the_value()</code></p>';
 		rwmb_the_value( $prefix . $field, [ 'object_type' => 'comment' ], $comment_id );
 		echo '<br><br><hr><br>';
